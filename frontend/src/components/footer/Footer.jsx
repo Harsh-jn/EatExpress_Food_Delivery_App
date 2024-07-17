5ff562f026272051c9447c3dfcc6ff1e84c6e355
harsh-jn <1810hj@gmail.com>
2024-07-17 21:10:01 +0530

    first commit

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./Footer.css";
+import { assets } from '../../assets/assets';
+export const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+          Our technology platform connects customers, restaurant partners and delivery partners, serving their multiple needs. Customers use our platform to search and discover restaurants, read and write customer generated reviews and view and upload photos, order food delivery, book a table and make payments while dining-out at restaurants.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>Company</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+91 9982083199</li>
+                <li>contact@EatExpress.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className='footer-copyrights'>copyrights 2024 c EatExpress.com - All Rights Reserved.</p>
+    </div>
+  );
+}
