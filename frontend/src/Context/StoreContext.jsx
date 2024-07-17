5a1edc5cd5d8dedb53386ef2db7e8a692d38b838
Harsh Jain <97253124+Harsh-jn@users.noreply.github.com>
2024-07-17 22:55:47 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -3,7 +3,7 @@ export const StoreContext = createContext(null);
 import axios from "axios"
 const StoreContextProvider = (props) => {
   const[cartItems,setCartItems]= useState({})
-  const url="http://localhost:4000";
+  const url="https://eatexpress-food-delivery-app-1.onrender.com";
   const[token,setToken]=useState("")
   const[food_list,Setfood_list]=useState([])
   const addToCart = async(itemId)=>{
